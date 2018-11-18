6f7f18fd188170a31c229053389c5b534939ae88
Brian Zulch <lego.trains.b@gmail.com>
2018-11-18 12:26:48 -0700

    Revert "Revert "Inital commit""
    This reverts commit 82d9015ea4ac59aedc545920c0c204b228a66d16.

---
+++ Mod9/Mod9/RunCmd.cs
@@ -0,0 +1,30 @@
+﻿using Smod2.Commands;
+
+namespace VirtualBrightPlayz.SCPSL.Mod9
+{
+    internal class RunCmd : ICommandHandler
+    {
+        private Mod9 mod9;
+
+        public RunCmd(Mod9 mod9)
+        {
+            this.mod9 = mod9;
+        }
+
+        string ICommandHandler.GetCommandDescription()
+        {
+            return "Scans for all Class-D's";
+        }
+
+        string ICommandHandler.GetUsage()
+        {
+            return "dscan";
+        }
+
+        string[] ICommandHandler.OnCall(ICommandSender sender, string[] args)
+        {
+            Mod9EventHandler.pTime = 0;
+            return new string[] { "Scanning for Class-D" };
+        }
+    }
+}
